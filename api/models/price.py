dc6e0c16a95f1d2499d269e585134a6656fda07d
makrron <mvazquezarenal@uma.es>
2023-07-19 20:12:55 +0200

    ADD: data types

--- api/models/price.py
+++ api/models/price.py
@@ -5,13 +5,13 @@
 class Price:
     """Class representing the Bitcoin Price model in fiat."""
 
-    def __init__(self, price, pair, exchange, timestamp):
+    def __init__(self, price: float, pair: str, exchange: str, timestamp):
         self.price = price
         self.pair = pair
         self.exchange = exchange
         self.timestamp = timestamp
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         """
         Return a dictionary representation of the object.
         Returns: dict
