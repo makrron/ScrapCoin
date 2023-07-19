821855f01867d901da75364009d35b758888a061
makrron <mvazquezarenal@uma.es>
2023-07-19 20:11:15 +0200

    ADD: home route

--- api/controllers/price_controller.py
+++ api/controllers/price_controller.py
@@ -1,1 +1,10 @@
 """Specific driver for the endpoint that returns Bitcoin prices in fiat."""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'message': 'Welcome to the ScrapCoin API!'})
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 """Main file where the application will be started and the initial paths and configurations will be defined."""
 
-if __name__ == '__main__':
-    print("Hi!")
+from api.controllers import price_controller
+
+app = price_controller.app
+
