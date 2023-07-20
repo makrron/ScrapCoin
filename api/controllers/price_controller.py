e1ff4b1382e2c56ba2f09822fada654d2532e39e
makrron <mvazquezarenal@uma.es>
2023-07-20 18:21:56 +0200

    ADD: New endpoints to get more information about prices and exchanges

--- api/controllers/price_controller.py
+++ api/controllers/price_controller.py
@@ -83,3 +83,65 @@ def get_exchange_price_list(exchange_name):
             return jsonify(rows), 200
 
 
+@app.route('/exchanges', methods=['GET'])
+def get_exchange_list():
+    """
+    Returns the list of exchanges available.
+    :return: JSON object with the list of exchanges available.
+    """
+
+    conn = get_db_connection()
+    if request.method == 'GET':
+        cur = conn.execute(f"SELECT DISTINCT EXCHANGE FROM BITCOIN").fetchall()
+
+        rows = [dict(row) for row in cur]
+
+        if len(rows) == 0:
+            return jsonify({'ERROR': 'Invalid exchange.'}), 400
+        else:
+            return jsonify(rows), 200
+
+
+@app.route('/exchanges/<exchange_name>/pairs', methods=['GET'])
+def get_exchange_pair_list(exchange_name):
+    """
+    Returns the list of trading pairs available on a specific exchange,
+    e.g. GET /exchanges/CoinBasePro/pairs to get the trading pairs available on Coinbase Pro.
+    :param exchange_name: The name of the exchange.
+    :return: JSON object with the list of trading pairs available on the given exchange.
+    """
+
+    conn = get_db_connection()
+    if request.method == 'GET':
+        cur = conn.execute(
+            f"SELECT DISTINCT PAIR FROM BITCOIN WHERE EXCHANGE LIKE '{exchange_name.upper()}'").fetchall()
+
+        rows = [dict(row) for row in cur]
+
+        if len(rows) == 0:
+            return jsonify({'ERROR': 'Invalid exchange.'}), 400
+        else:
+            return jsonify(rows), 200
+
+
+@app.route('/exchanges/<exchange_name>/pairs/<pair>', methods=['GET'])
+def get_exchange_pair_price(exchange_name, pair):
+    """
+    Returns the price of a specific trading pair on a specific exchange,
+    e.g. GET /exchanges/CoinBasePro/pairs/BTC_USD to get the price of Bitcoin in US dollars on Coinbase Pro.
+    :param exchange_name: The name of the exchange.
+    :param pair: The trading pair to be searched.
+    :return: JSON object with the price of the given trading pair on the given exchange.
+    """
+
+    conn = get_db_connection()
+    if request.method == 'GET':
+        cur = conn.execute(
+            f"SELECT * FROM BITCOIN WHERE EXCHANGE LIKE '{exchange_name.upper()}' AND PAIR LIKE '{pair.upper()}'").fetchall()
+
+        rows = [dict(row) for row in cur]
+
+        if len(rows) == 0:
+            return jsonify({'ERROR': 'Invalid exchange or pair.'}), 400
+        else:
+            return jsonify(rows), 200
