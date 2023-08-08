e82ede53bb196aa537fb5948d17e59dd64163fa0
makrron <mvazquezarenal@uma.es>
2023-08-08 21:35:24 +0200

    CHANGE: Put the api in production

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Main file where the application will be started and the initial paths and configurations will be defined."""
 
 from api.controllers import routes
+from waitress import serve
 
 app = routes.app
-
+serve(app, host='0.0.0.0', port=8080)
