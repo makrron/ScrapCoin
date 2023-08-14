d71309d0821f9c690bcdd655ac05a3420b62ccef
makrron <mvazquezarenal@uma.es>
2023-08-14 20:52:14 +0200

    FIX: Minor fixings

--- api/utils/web_scraper.py
+++ api/utils/web_scraper.py
@@ -88,6 +88,8 @@ def create_driver() -> webdriver:
     options.add_argument("--start-maximized")
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-dev-shm-using")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-setuid-sandbox")
     options.add_argument('--headless')
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
