4bb86b7a0771dd043dd5eed2c34a8cc68f9c341d
makrron <mvazquezarenal@uma.es>
2023-08-14 22:47:14 +0200

    FIX: Minor fixings

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 """Main file where the application will be started and the initial paths and configurations will be defined."""
+import json
 
 from waitress import serve
 
 from api.controllers import routes
 
+with open("instance/config.json", "r") as f:
+    config = json.load(f)
+
 app = routes.app
 
 # Start the Flask app using Waitress
-serve(app, host='0.0.0.0', port=8080)
+serve(app, host=config['HOST'], port=config['PORT'])
